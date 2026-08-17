58d8fcede312548e7ffd85f8988c6aab21ce82fc
Veniamin Medanov <veniamin.medanov@icloud.com>
2026-08-17 00:37:10 -0400

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { page } from '$app/state';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>{page.data.title ?? 'Veniamin Medanov'}</title>
+	<meta
+		name="description"
+		content={page.data.description ?? 'Veniamin is a 13 year old software engineer.'}
+	/>
+	<link rel="icon" href={favicon} />
+	<script src="https://kit.fontawesome.com/6292d5369d.js" crossorigin="anonymous"></script>
+</svelte:head>
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import Main from '$lib/components/Main.svelte';
+
+	let isHovering = $state(false);
+	const radius = 100;
+	let x = $state(0);
+	let y = $state(0);
+
+	function handlePointerMove(e: PointerEvent) {
+		const rect = (e.currentTarget as HTMLElement).getBoundingClientRect();
+
+		x = e.clientX - rect.left;
+		y = e.clientY - rect.top;
+	}
+</script>
+
+<div
+	class="relative min-h-screen w-screen bg-linear-to-br from-yellow-50 to-yellow-200"
+	onpointerenter={() => (isHovering = true)}
+	onpointerleave={() => (isHovering = false)}
+	onpointermove={handlePointerMove}
+	role="group"
+>
+	<!-- Hidden -->
+	<div class="absolute z-10 h-full w-full text-green-500">Congrats if you found this!</div>
+
+	<!-- Mask -->
+	<div
+		class="pointer-events-none absolute inset-0 z-20 h-full w-full bg-slate-950"
+		style={isHovering
+			? `
+			mask-image: radial-gradient(circle ${radius}px at ${x}px ${y}px, transparent 0%, black 100%);
+			-webkit-mask-image: radial-gradient(circle ${radius}px at ${x}px ${y}px, transparent 0%, black 100%);
+		`
+			: ''}
+	></div>
+
+	<main class="relative z-30 w-full">
+		<Main />
+	</main>
+</div>
